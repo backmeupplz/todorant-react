74d1b3c9b1a37d7781dd1d2e66ee9fc8af6d32d7
Ruslan Zapevalov <upacyxou@tutanota.com>
2021-12-11 00:03:49 +0400

    add wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Suspense } from 'react'
+import { Route } from 'wouter'
 import Landing from 'screens/Landing'
 import LocalizationProvider from 'localization/LocalizationProvider'
 import Navbar from 'components/Navbar'
-import React from 'preact/compat'
+import ProtectedRoute from 'components/ProtectedRoute'
 import Root from 'components/Root'
+import Superpower from 'screens/Superpower'
 
 const App = () => {
   return (
     <Root>
       <LocalizationProvider>
         <Navbar />
-        <Landing />
+        <Route path="/" component={Landing} />
+        <ProtectedRoute path="/superpower" component={Superpower} />
       </LocalizationProvider>
     </Root>
   )
