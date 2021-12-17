4e8ecef63479ecd157b4383843eaec0b93fb9f81
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2021-12-17 19:04:21 +0300

    Add video localization

--- src/components/Landing/Video.tsx
+++ src/components/Landing/Video.tsx
@@ -1,4 +1,8 @@
 import { classnames } from 'classnames/tailwind'
+import { useSnapshot } from 'valtio'
+import AppStore from 'stores/AppStore'
+import Language from 'models/Language'
+import useI18N from 'hooks/useI18N'
 
 const frame = classnames(
   'lg:w-video',
@@ -12,13 +16,13 @@ const frame = classnames(
 )
 
 const Video = () => {
-  return (
-    <iframe
-      title="How to use Todorant"
-      src="https://www.youtube.com/embed/lYXhqHt7_QY"
-      className={frame}
-    />
-  )
+  const { LL } = useI18N()
+  const { language } = useSnapshot(AppStore)
+  const source =
+    language === Language.ru
+      ? 'https://www.youtube.com/embed/heR0rlllTVg'
+      : 'https://www.youtube.com/embed/lYXhqHt7_QY'
+  return <iframe title={LL.howto.title()} src={source} className={frame} />
 }
 
 export default Video
